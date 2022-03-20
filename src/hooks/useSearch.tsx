12652bd6d9d03e95334e82146a1641095efef3d8
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2022-03-20 15:50:58 -0400

    added filter queries for language with custom checkboxes, created basic Card for search results

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,9 +1,19 @@
 import useSWR from "swr";
 import { getSearch } from "utils/api";
 
-export default function useSearch(searchTerm: string) {
-  const { data } = useSWR(["getSearch", searchTerm], (_, search_term) =>
-    getSearch({ starts_with: "team", search_term })
+export default function useSearch(searchTerm: string, language?: string) {
+  const { data } = useSWR(
+    ["getSearch", searchTerm, language],
+    (_, search_term, lang) =>
+      getSearch({
+        starts_with: "team",
+        search_term,
+        filter_query: lang
+          ? {
+              __or: [{ languages: { any_in_array: lang } }],
+            }
+          : null,
+      })
   );
 
   return { data };
