910f0f68e520e6a5a17089f2b691f60dd39744bd
Gia Thinh Nguyen <gthinh.nguyen.dev@gmail.com>
2024-03-03 12:48:59 -0500

    add job titles to leadership cards

--- src/components/About/Leadership.tsx
+++ src/components/About/Leadership.tsx
@@ -27,70 +27,107 @@ export default function Leadership() {
       titles: [
         t('role.founder', { gender: 'male' }),
         t('role.president', { gender: 'male' }),
+        t('job.physiotherapist', { gender: 'male' }),
       ],
       src: '/images/leadership/kimfay.png',
     },
     {
       name: 'Vida Sieu',
-      titles: [t('role.vice_president', { gender: 'female' })],
+      titles: [
+        t('role.vice_president', { gender: 'female' }),
+        t('job.occupational_therapist', { gender: 'female' }),
+      ],
       src: '/images/leadership/vida.png',
     },
     {
       name: 'Melissa Hua',
-      titles: [t('role.founder', { gender: 'female' }), t('role.secretary')],
+      titles: [
+        t('role.founder', { gender: 'female' }),
+        t('role.secretary'),
+        t('job.medical_resident', { gender: 'female' }),
+      ],
       src: '/images/leadership/melissa.png',
     },
     {
       name: 'Gillian Truong',
-      titles: [t('role.treasurer', { gender: 'female' })],
+      titles: [
+        t('role.treasurer', { gender: 'female' }),
+        t('job.application_engineer', { gender: 'female' }),
+      ],
     },
     {
       name: 'Belinda Jiao',
-      titles: [t('role.marketing', { gender: 'female' })],
+      titles: [
+        t('role.marketing', { gender: 'female' }),
+        t('job.medical_resident', { gender: 'female' }),
+      ],
       src: '/images/leadership/belinda.png',
     },
     {
       name: 'Alex Chung',
-      titles: [t('role.marketing', { gender: 'male' })],
+      titles: [
+        t('role.marketing', { gender: 'male' }),
+        t('job.healthcare_student', { gender: 'male' }),
+      ],
       src: '/images/leadership/alex.png',
     },
     {
       name: 'Uyen Do',
-      titles: [t('role.volunteers', { gender: 'female' })],
+      titles: [
+        t('role.volunteers', { gender: 'female' }),
+        t('job.research_officer', { gender: 'female' }),
+      ],
       src: '/images/leadership/uyen.png',
     },
 
     {
       name: 'Caroline Trinh',
-      titles: [t('role.community_outreach', { gender: 'female' })],
+      titles: [
+        t('role.community_outreach', { gender: 'female' }),
+        t('job.social_work_student', { gender: 'female' }),
+      ],
       src: '/images/leadership/caroline.png',
     },
     {
       name: 'Robert Chen',
-      titles: [t('role.community_outreach', { gender: 'male' })],
+      titles: [
+        t('role.community_outreach', { gender: 'male' }),
+        t('job.accountant', { gender: 'male' }),
+      ],
       src: '/images/leadership/robert.png',
     },
 
     {
       name: 'Brendon Pham',
-      titles: [t('role.founder', { gender: 'male' }), t('role.member')],
+      titles: [
+        t('role.founder', { gender: 'male' }),
+        t('role.member'),
+        t('job.physiotherapist', { gender: 'male' }),
+      ],
       src: '/images/leadership/brendon.png',
     },
 
     {
       name: 'Kaiyang Li',
-      titles: [t('role.member')],
+      titles: [
+        t('role.member'),
+        t('job.medical_resident', { gender: 'female' }),
+      ],
       src: '/images/leadership/kaiyang.png',
     },
     {
       name: 'Yu Xin Hu',
-      titles: [t('role.founder', { gender: 'female' }), t('role.member')],
+      titles: [
+        t('role.founder', { gender: 'female' }),
+        t('role.member'),
+        t('job.physiotherapist', { gender: 'female' }),
+      ],
       src: '/images/leadership/yuxin.png',
     },
 
     {
       name: 'Oliver Ying Zhan',
-      titles: [t('role.member')],
+      titles: [t('role.member'), t('job.medical_resident', { gender: 'male' })],
       src: '/images/leadership/oliver.png',
     },
   ]
@@ -126,7 +163,9 @@ export default function Leadership() {
               key={member.name}
               flexDirection={{ base: 'column', md: 'row' }}
               alignItems="center"
-              gap={2}
+              justifyContent="center"
+              flex={{ base: 1, md: 0 }}
+              gap={{ base: 4, md: 2 }}
             >
               <Avatar
                 size="2xl"
@@ -147,8 +186,14 @@ export default function Leadership() {
                 marginTop={{ base: 3, md: 30 }}
                 marginBottom={{ md: 'auto' }}
                 alignItems={{ base: 'center', md: 'flex-start' }}
+                justifyContent="center"
+                textAlign={{ base: 'center', md: 'start' }}
               >
-                <Divider borderWidth="1px" borderColor="primary.500" />
+                <Divider
+                  borderWidth="1px"
+                  borderColor="primary.500"
+                  width={100}
+                />
                 <Text fontWeight="bold" pb={1}>
                   {member.name}
                 </Text>
