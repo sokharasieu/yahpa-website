b6484c5061b8ad368f0df58e21f6b64ec0152acb
sokharasieu <sokhara.sieu@gmail.com>
2025-01-06 15:03:45 -0500

    d

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,14 +1,15 @@
-// filename: cypress/integration/test_spec.js
+
 describe('Language Switch Test', () => {
   it('Visits yahapa.org and switches the language to Vietnamese', () => {
+
     // Visit the website
     cy.visit('https://yahpa.org');
 
-    // Find and click the language switcher (assuming it has an id or class you can target)
-    cy.get('.css-1gpsbw3').select('vi'); // Assuming 'vi' is the value for Vietnamese
+    // Find the language switcher element and select Vietnamese
+    cy.get('.css-1gpsbw3').select('vi');
 
     // Verify that the language has been switched to Vietnamese
-    // This can be done by checking for a specific element that changes with the language
-    cy.get('body').should('contain', 'Tiếng Việt'); // Adjust the selector and text as needed
+    cy.get('body').should('contain', 'Tiếng Việt'); 
   });
 });
+//
